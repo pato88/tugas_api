127adca3a74bb4bf918eb2f6c83985f6c2165f16
pato88 <amarmuktasim87@gmail.com>
2024-05-09 00:00:09 +0700

    tugas-api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from "./api"
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMoviesList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className='Movie-wrapper' key={i}>
+          <div className='Movie-title'>{movie.title}</div>
+          <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className='Movie-date'> realease : {movie.release_date}</div>
+          <div className='Movie-rate'>{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = (q) => {
+    console.log({ q })
+  }
+
+  console.log({ popularMovies: popularMovies })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Amar Movie Mania</h1>
+        <input placeholder='cari film disini' className='Movie-search' onChange={({ target }) => search(target.value)} />
+        <div className="Movie-container">
+          <PopularMoviesList />
+        </div>
+
       </header>
     </div>
   );
